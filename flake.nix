a9967b91e733de627ae5390ddf5879949e81b322
Eric Tossell <erictossell@gmail.com>
2024-02-02 17:40:00 -0500

    Readme-py

--- flake.nix
+++ flake.nix
@@ -4,13 +4,14 @@
   inputs = {
     eriixvim.url = "github:erictossell/eriixvim";
     go-time.url = "github:erictossell/go-time";
+    readme-py.url = "github:erictossell/readme-py";
     russh.url = "github:erictossell/russh";
     homepage-nix.url = "github:erictossell/homepage-nix";
     nixpkgs.url = "nixpkgs/nixos-unstable"; 
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, eriixvim, nixpkgs, flake-utils, go-time, russh, homepage-nix, ... }:
+  outputs = { self, eriixvim, nixpkgs, flake-utils, go-time, russh, homepage-nix, readme-py, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
@@ -20,6 +21,7 @@
         packages = {
 	  eriixvim = eriixvim.packages.${system}.default;
           go-time = go-time.packages.${system}.default;
+	  readme-py = readme-py.packages.${system}.default;
           russh = russh.packages.${system}.default;
           homepage-nix = homepage-nix.packages.${system}.homepage-nix;
         };
